05233ddb3528197a2c23ed417597f087c20cdee5
suirham <151624478+suirham@users.noreply.github.com>
2023-11-21 14:57:34 +0100

    Update and rename digispark.sh to digispark2.sh

--- digispark.sh
+++
@@ -1,4 +0,0 @@
- cd ~\Desktop\
- New-Item virus.omg
- Add-Content .\virus.omg "mouhaha je t haké petit gouniafier"
- notepad .\virus.omg
---
+++ digispark2.sh
@@ -0,0 +1,8 @@
+cd ~\Desktop\
+New-Item virus.omg
+while($val -ne 5)
+{
+    $val++
+     Add-Content .\virus.omg "mouhaha je t haké petit gouniafier"
+ notepad .\virus.omg
+}
